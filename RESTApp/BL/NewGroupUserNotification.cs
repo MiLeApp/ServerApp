b47477c6cf615de969abc425257b2f3cb885b776
Yoni <ytarlovs@gmail.com>
2017-06-19 15:26:38 +0300

    Added FireBaseAccessLayer

---
+++ RESTApp/BL/NewGroupUserNotification.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RESTApp.BL
+{
+    public class UserNotification
+    {
+        private List<object> notifObjects = new List<object>();
+        public int OpCode { get; set; }
+        public string Title { get; set; }
+        public string ReceiverFBId { get; set; }
+        public List<Object> NotificationObj
+        {
+            get
+            {
+                return notifObjects;
+            }
+            set
+            {
+                notifObjects = value;
+            }
+        }
+    }
+}
